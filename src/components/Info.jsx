bb757e1ce8b16258f5856b274ef7e130221d3f70
iamkamalraj <kamalrajp.cse@gmail.com>
2023-12-09 20:06:18 +0530

    info minor allignment

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,13 +9,13 @@ const Info = ({title,img,description}) => {
           to="/"
           className="flex items-center gap-2 my-5"
         >
-          <img src={backIcon} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={backIcon} alt="logo" className="ml-12 w-9 h-9 object-contain" />
         </Link>
       </nav>
       <div className="flex flex-row">
-        <img className="aspect-w-3 aspect-h-5 h-[80dvh] mx-5 my-0" src={img}/>
-        <div className="flex flex-col justify-start items-start">
-          <h3 className=" text-2xl w-[100%] text-center font-bold ">{title}</h3>
+        <img className="ml-40 aspect-w-3 aspect-h-5 h-[80dvh] mx-5 my-0" src={img}/>
+        <div className="ml-10 mr-40 flex flex-col justify-start items-start">
+          <h3 className="mb-8 text-4xl w-[100%] text-center font-bold ">{title}</h3>
           {description.map((text,index)=> <p className="text-center my-3" key={index}>{text}</p>)}
         </div>
       </div>
