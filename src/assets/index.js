985759d8a40d5e064fc589d7ee12e6c4c7d57e60
iamkamalraj <kamalrajp.cse@gmail.com>
2024-01-07 08:20:34 +0530

    image format changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,28 +1,28 @@
-import logo from "./logo.png";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
+import logo from "./logo.webp";
+import backend from "./backend.webp";
+import creator from "./creator.webp";
+import mobile from "./mobile.webp";
+import web from "./web.webp";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
+import css from "./tech/css.webp";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
+import html from "./tech/html.webp";
+import javascript from "./tech/javascript.webp";
+import mongodb from "./tech/mongodb.webp";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import visual1 from "./tech/visual1.png";
-import visual2 from "./tech/visual2.png";
-import visual3 from "./tech/visual3.png";
+import visual1 from "./tech/visual1.webp";
+import visual2 from "./tech/visual2.webp";
+import visual3 from "./tech/visual3.webp";
 // import pred1 from "./tech/pred1.png";
 // import pred1 from "./tech/pred1_1.png";
 // import pred1 from "./tech/pred1_2.png";
@@ -32,17 +32,17 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import carrent from "./carrent.webp";
+import jobit from "./jobit.webp";
+import tripguide from "./tripguide.webp";
 
 // import mona_lisa from "./mona_lisa.jpg";
-import vis_dis1 from "./vis_dis1.png";
-import vis_dis2 from "./vis_dis2.png";
-import vis_dis3 from "./vis_dis3.png";
-import vis_dis13 from "./vis_dis13.png";
-import vis_dis14 from "./vis_dis14.png";
-import vis_dis15 from "./vis_dis15.png";
+import vis_dis1 from "./vis_dis1.webp";
+import vis_dis2 from "./vis_dis2.webp";
+import vis_dis3 from "./vis_dis3.webp";
+import vis_dis13 from "./vis_dis13.webp";
+import vis_dis14 from "./vis_dis14.webp";
+import vis_dis15 from "./vis_dis15.webp";
 
 export {
   logo,
